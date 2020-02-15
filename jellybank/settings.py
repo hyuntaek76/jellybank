aae9e975cd2d3d6d9e3a4aec7ded1a6e000e7693
hyuntaek lim <corey76@hyuntaekui-MacBookPro.local>
2020-02-15 13:35:11 +0900

    stat_root add

--- jellybank/settings.py
+++ jellybank/settings.py
@@ -123,6 +123,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media',)
@@ -164,4 +166,3 @@ SUMMERNOTE_CONFIG = {
     },
 
 }
-
